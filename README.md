4defa3b413a21a7b401275f236723a772097fa69
Shakti Singh Rathore <43042984+ccfahe@users.noreply.github.com>
2025-05-09 05:52:24 +0000

    Training pipeline added

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # mlops-zoom-camp
+
+
+Setup:
+1.Github accound creation
+2.Gitgub codespace
+3.Anacond installation
+
+Data science
+1.Create jupyter notebbok
+2.Downlaod data and train model
+3.create model
+
+MLOPS satarting
+
+1.create requiremnt.txt
+
+mlflow
+jupyter
+scikit-learn
+pandas
+seaborn
+hyperopt
+xgboost
+2.conda create -n exp-tracking-env python=3.9
+3.conda activate exp-tracking-env
+4.mlflow ui --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlruns
+
+
+
+USe notebook
+
+to set uri 
+set experiment name 
---
+++ start_mlflow.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+conda activate exp-tracking-env
+cd 03-training/experiment_tracking/
+
+# Start MLflow UI in background
+mlflow ui --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlruns &
+MLFLOW_PID=$!
+
+# Run Jupyter notebook non-interactively
+#jupyter nbconvert --to notebook --execute pipeline.ipynb --output pipeline1.ipynb
+
+# Kill MLflow after notebook execution (optional)
+#kill $MLFLOW_PID
+
+# If you want to pass parameter 
+:'
+
+
+#!/bin/bash
+
+input_file = "default.csv"
+alpha = 0.01
+conda activate exp-tracking-env
+cd 03-training/experiment_tracking/
+
+# Start MLflow UI in background
+mlflow ui --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlruns &
+MLFLOW_PID=$!
+
+# Run notebook with papermill and pass parameters
+papermill your_notebook.ipynb output_notebook.ipynb \
+    -p input_file "green_tripdata_2021-02.parquet" \
+    -p alpha 0.1
+
+# Kill MLflow after notebook execution (optional)
+kill $MLFLOW_PID
+'
