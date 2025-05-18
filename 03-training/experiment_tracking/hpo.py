e488561030fe0d09953aded4ea542494cf8e2bc0
shakti <ccfahe@example.com>
2025-05-18 01:29:28 +0000

    Added mlops content

--- 03-training/experiment_tracking/hpo.py
+++ 03-training/experiment_tracking/hpo.py
@@ -38,7 +38,7 @@ def run_optimization(data_path: str, num_trials: int):
         rf = RandomForestRegressor(**params)
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
-        rmse =  rmse = mean_squared_error(y_val, y_pred) ** 0.5
+        rmse = mean_squared_error(y_val, y_pred) ** 0.5
 
         return {'loss': rmse, 'status': STATUS_OK}
 
